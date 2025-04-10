711df546070493e1001269420dd32a1dcecf5244
ncb <ncb436@educa.madrid.org>
2025-04-10 16:43:59 +0200

    creacion de java

---
+++ Vocales.java
@@ -0,0 +1,22 @@
+/*
+ * Solicita al usuario una cadena y comprueba el número de vocales que tiene.
+ */
+package programacionbasica;
+
+import java.util.Scanner;
+
+public class Vocales {
+
+    public static void main(String[] args) {
+        String cadena = "";
+        int contador = 0; //Contador de vocales
+
+        //Solicita al usuario una palabra teclado y lo almacena en la variable
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce una palabra: ");
+        cadena = sc.nextLine();
+        sc.close();
+//añadir codigo
+        System.out.println("La palabra "+cadena+" contiene "+contador+" vocales.");
+    }
+}
